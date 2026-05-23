8367bd5bed054a940ffb5d249877267bc9f70bdc
He Wang <whfreedom128@gmail.com>
2026-05-23 11:57:56 +1000

    Add docker PostgreSQL container

--- AiKnowledgeCopilot.API/Program.cs
+++ AiKnowledgeCopilot.API/Program.cs
@@ -1,4 +1,13 @@
+using AiKnowledgeCopilot.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // Add services to the container.
 
